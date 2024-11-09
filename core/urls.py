dad2f73759e68df550c3dbe27ff0ef3b5e0da6cd
Aydin Salar <aydin.s@yahoo.com>
2024-11-09 16:17:31 -0500

    Update Feedback model field and apply migrations

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,12 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, SkillViewSet, SessionViewSet, FeedbackViewSet
+from .views import UserProfileViewSet, SkillViewSet, SessionViewSet, MatchViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserProfileViewSet)
 router.register(r'skills', SkillViewSet)
 router.register(r'sessions', SessionViewSet)
-router.register(r'feedback', FeedbackViewSet)
+router.register(r'matches', MatchViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
