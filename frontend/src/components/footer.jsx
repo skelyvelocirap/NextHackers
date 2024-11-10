ba6f179bcb7707b8b17a332d63358e1cc0775cc5
Neo Sommer <sommer.neo17@gmail.com>
2024-11-09 22:35:05 -0700

    Revert "Merge branch 'front-end' into test"
    This reverts commit 95c0f01b9ffb02758ceeaa89afa43a39f791b7c0, reversing
    changes made to 4907ad9316d851c27d8aea07a5e8c216277d5071.

--- frontend/src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import Logo from "./logo";
-
-export default function Footer() {
-  return (
-    <footer className="w-full border-t border-border p-6 flex flex-col justify-center items-center">
-      <Logo />
-    </footer>
-  );
-}
