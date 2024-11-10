b8af96529268bc6db8a7deeb420ef8a526550338
Neo Sommer <sommer.neo17@gmail.com>
2024-11-10 11:01:24 -0700

    Setup login page

---
+++ frontend/src/app/auth/login/page.js
@@ -0,0 +1,14 @@
+export default function LoginPage(){
+    return (
+    <div className="">
+        <section className="section pt-40">
+            <div className="flex flex-col justify-center items-center text-center max-w-2xl mx-auto">
+                <h1 className="heading-1 mb-2">Login</h1>
+                <form>
+                    
+                </form>
+            </div>
+        </section>
+    </div>
+    )
+}
--- frontend/src/components/profile-card.jsx
+++ frontend/src/components/profile-card.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { Badge } from "./ui/badge";
 import {
   Card,
   CardContent,
@@ -9,37 +8,104 @@ import {
 } from "./ui/card";
 import SkillTag from "./skill-tag";
 import Image from "next/image";
+import { skills as allSkills } from "@/lib/dummy-data";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
 
-export default function ProfileCard({ name, title, image, skills }) {
+export default function ProfileCard({
+  name,
+  title,
+  image,
+  skills,
+  id,
+  bio,
+  volunteerHours,
+}) {
+  const userSkills = skills
+    .map((skillId) => allSkills.find((skill) => skill.skillId === skillId))
+    .filter((skill) => skill !== undefined); // Filter out undefined skills
+
   return (
-    <Link href="/" className="">
-      <Card className="hover:scale-[103%] transition-all duration-200 ease-out hover:shadow-lg h-full flex flex-col justify-between items-center">
-        <CardHeader className="flex flex-col gap-1 justify-start items-center text-center">
-          <div className="relative size-20 rounded-full bg-slate-400 shadow-inner overflow-hidden">
-            <Image
-              className="absolute object-cover"
-              fill
-              src={image}
-              alt={name}
-            />
-          </div>
-          <div>
-            <CardTitle className="text-lg font-semibold">{name}</CardTitle>
-            <CardDescription>{title}</CardDescription>
-          </div>
-        </CardHeader>
-        <CardContent>
-          {skills && (
-            <div className="flex flex-wrap gap-2">
-              {skills.map((skill) => (
-                <SkillTag key={skill.skillId} varient="small">
-                  {skill.skillName}
-                </SkillTag>
-              ))}
+    <Dialog>
+      <DialogTrigger>
+        <Card className="hover:scale-[103%] transition-all duration-200 ease-out hover:shadow-lg h-full flex flex-col justify-between items-center">
+          <CardHeader className="flex flex-col gap-1 justify-start items-center text-center">
+            <div className="relative size-20 rounded-full bg-slate-400 shadow-inner overflow-hidden">
+              <Image
+                className="absolute object-cover"
+                fill
+                src={image}
+                alt={name}
+              />
             </div>
-          )}
-        </CardContent>
-      </Card>
-    </Link>
+            <div>
+              <CardTitle className="text-lg font-semibold">{name}</CardTitle>
+              <CardDescription>{title}</CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent>
+            {userSkills && (
+              <div className="flex flex-wrap gap-2">
+                {userSkills.map((skill) => (
+                  <SkillTag key={skill.skillId} varient="small">
+                    {skill.skillName}
+                  </SkillTag>
+                ))}
+              </div>
+            )}
+          </CardContent>
+        </Card>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <div className="flex flex-col items-center gap-4 justify-center p-6 max-w-xl mx-auto">
+            <div className="flex flex-col justify-center items-center gap-4 text-center">
+              <div className="relative size-24 rounded-full bg-slate-400 shadow-inner overflow-hidden">
+                <Image
+                  className="absolute object-cover"
+                  fill
+                  src={image}
+                  alt={name}
+                />
+              </div>
+              <div>
+                <DialogTitle className="font-semibold text-lg">
+                  {name}
+                </DialogTitle>
+                <p className="text-sm mb-4">{title}</p>
+                <p className="mb-6">{bio}</p>
+              </div>
+            </div>
+            <div className="mb-6">
+              <h3 className="font-semibold text-center mb-4">
+                Skills I teach:
+              </h3>
+              <ul>
+                {userSkills && (
+                  <div className="flex flex-wrap gap-2">
+                    {userSkills.map((skill) => (
+                      <SkillTag key={skill.skillId} varient="small">
+                        {skill.skillName}
+                      </SkillTag>
+                    ))}
+                  </div>
+                )}
+              </ul>
+            </div>
+            <div className="flex justify-center items-center gap-4">
+              <Button>Book session</Button>
+              <Button variant="outline">Message</Button>
+            </div>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
